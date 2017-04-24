6bd1661f61e09f501d0fad0c83b0f1f6f196caa3
Chelsea Weber <chelseaweber92@gmail.com>
2017-04-24 15:30:33 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	
+	//fade opacity on hover
+	$('#nav-bar li').mouseenter(function() {
+		$(this).fadeTo('fast', 0.25)
+	}).mouseleave(function(){
+		$(this).fadeTo('fast', 1)
+	});
+
+	//change background when clicked
+	$('#nav-bar li').mousedown(function() {
+		$(this).css('background-color', '#A26769')
+	}).mouseup(function(){
+		$(this).css('background-color', '#CB807D')
+	});
+	
+	//add 'Clicked!' after clicked
+	$('#nav-bar li').click(function(){	
+		$(this).removeClass('unclicked').addClass('clicked');
+	});
+	
+	
+});
